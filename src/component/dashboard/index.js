759bd7f2fc13a673ab362557b42adff880178b05
rojanbade <rojan.bade@gmail.com>
2025-02-07 12:05:38 +0545

    Dashboard component integrated

---
+++ src/component/dashboard/index.js
@@ -0,0 +1,52 @@
+import Nav from "./nav";
+import React from "react";
+
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+const { Meta } = Card;
+
+const Dashboard = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <Nav />
+        <Card
+          style={{
+            width: 300,
+            marginTop: 16,
+          }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+            }
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
